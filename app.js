0ac0a19be384a77db45465fd2868c0ff8237b8f9
olivercharlie30 <olivercharlieglapinig@gmail.com>
2025-10-07 07:45:34 +0800

    basic function syntax typecripts

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var InputNum1 = document.querySelector("#input1");
-var InputNum2 = document.querySelector("#input2");
-var SubmitButton = document.querySelector("button");
-function addNumbers(num1, num2) {
-    return num1 - num2;
-}
-;
-SubmitButton === null || SubmitButton === void 0 ? void 0 : SubmitButton.addEventListener("click", function () {
-    console.log("The Number: ".concat(addNumbers(parseInt(InputNum1.value), parseInt(InputNum2.value)), " "));
-});
+// const InputNum1 = document.querySelector("#input1") as HTMLInputElement;
+// const InputNum2 = document.querySelector("#input2") as HTMLInputElement;
+// const SubmitButton = document.querySelector("button");
+// function addNumbers(num1 :number, num2 :number){
+//     return num1 - num2;
+// };
+// SubmitButton?.addEventListener("click", ()=> {
+//     console.log(`The Number: ${addNumbers(parseInt(InputNum1.value), parseInt(InputNum2.value))} `);
+// });
+// function syntaxFunction(number1 :number, number2 :number, ShowResult :boolean, message :string) {
+//     const result = number1 * number2;
+//     if(ShowResult){
+//         console.log(`${message} ${result}`);
+//     }else{
+//         console.log("error result...");
+//     }
+// }
+// const Num1 = 53;
+// const Num2 = 35.25;
+// const ShowResult = true;
+// const message = "Compute the number:";
+// console.log(syntaxFunction(Num1, Num2, ShowResult, message));
+var number1;
+number1 = 5;
+var number2;
+number2 = 10;
+console.log(number1 + number2);
--- app.ts
+++ app.ts
@@ -1,13 +1,41 @@
-const InputNum1 = document.querySelector("#input1") as HTMLInputElement;
-const InputNum2 = document.querySelector("#input2") as HTMLInputElement;
-const SubmitButton = document.querySelector("button");
+// const InputNum1 = document.querySelector("#input1") as HTMLInputElement;
+// const InputNum2 = document.querySelector("#input2") as HTMLInputElement;
+// const SubmitButton = document.querySelector("button");
 
-function addNumbers(num1 :number, num2 :number){
+// function addNumbers(num1 :number, num2 :number){
 
-    return num1 - num2;
-};
+//     return num1 - num2;
+// };
 
-SubmitButton?.addEventListener("click", ()=> {
-    console.log(`The Number: ${addNumbers(parseInt(InputNum1.value), parseInt(InputNum2.value))} `);
-});
+// SubmitButton?.addEventListener("click", ()=> {
+//     console.log(`The Number: ${addNumbers(parseInt(InputNum1.value), parseInt(InputNum2.value))} `);
+// });
 
+
+
+// function syntaxFunction(number1 :number, number2 :number, ShowResult :boolean, message :string) {
+//     const result = number1 * number2;
+
+//     if(ShowResult){
+//         console.log(`${message} ${result}`);
+//     }else{
+//         console.log("error result...");
+//     }
+// }
+
+// const Num1 = 53;
+// const Num2 = 35.25;
+// const ShowResult = true;
+// const message = "Compute the number:";
+
+// console.log(syntaxFunction(Num1, Num2, ShowResult, message));
+
+
+
+let number1 :number;
+number1 = 5;
+
+let number2 :number;
+number2 = 10;
+
+console.log(number1 + number2);
